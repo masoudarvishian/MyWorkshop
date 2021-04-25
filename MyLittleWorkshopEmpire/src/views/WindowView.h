aad4b0cab5c1de10baee37fd11a42f0c7fdbc99f
Masoud Darvishian <masoudarvishian@gmail.com>
2021-04-25 12:22:18 +0430

    import glew & init imgui

--- MyLittleWorkshopEmpire/src/views/WindowView.h
+++ MyLittleWorkshopEmpire/src/views/WindowView.h
@@ -1,7 +1,12 @@
 #ifndef WINDOW_VIEW_H
 #define WINDOW_VIEW_H
 
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#include "../vendor/imgui/imgui.h"
+#include "../vendor/imgui/imgui_impl_glfw_gl3.h"
+
 #include "ViewBase.h"
 #include <iostream>
 
